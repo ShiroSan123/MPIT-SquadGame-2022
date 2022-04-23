cb16b5ce36e182507e0125ce158da86e90f0a4fc
ShiroSan123 <ShiruiSan@yandex.ru>
2022-04-23 15:36:45 +0900

    Relise

--- indexRegAndSign.php
+++ indexRegAndSign.php
@@ -11,7 +11,7 @@ $connect = mysqli_connect("127.0.0.1",root,"","MPIT-SquadGame-2022");
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="asset/css/style.css">
 </head>
-<body style="background-image: url(asset/img/MainBG.jpg)">
+<body>
 	<!-- Проверка на правильность регистрации или авторизации -->
 	<?php if ($_SESSION['RegAndSign'] == 1) { ?>
 		<!-- Кнопка назад в index.php -->
